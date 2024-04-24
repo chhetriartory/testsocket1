1c8a9043865e3a790b351c57f4fbce2b4599ab90
staff <okamotokabuto@Anil.local>
2024-04-24 16:11:46 +0900

    innitional

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require('cors')
+const path = require("path");
+const app = express();
+const server = require("http").createServer(app);
+
+app.use(cors({
+    origin: '*'
+}));
+
+// const io = require("socket.io")(server);
+const io = require('socket.io')(server, { 
+    cors: { origin: '*' } // for Cross-Origin Request Blocked
+});
+app.use(express.static(path.join(__dirname+"/public")));
+ console.log("test")
+io.on("connection", function(socket){
+    console.log("conected");
+    socket.on("newuser",function(username){
+        socket.broadcast.emit("update", username + "joind the conversation");
+    });
+    socket.on("exituser",function(username){
+        socket.broadcast.emit("update", username + "left the conversation");
+    });
+    socket.on("chat",function(message){
+        socket.broadcast.emit("chat", message );
+    })
+})
+
+
+server.listen(5000);
